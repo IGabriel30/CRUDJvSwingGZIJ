d46d017a9be6346af9919224562825c662bb14d6
Ingrid Jacqueline Gabriel Zepeda <jacquelinegabriel05@hotmail.com>
2024-06-08 01:36:41 -0600

    entidades

---
+++ src/main/java/entidades/Pedidos.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidades;
+
+import java.util.Date;
+
+/**
+ *
+ * @author IGABRIEL
+ */
+public class Pedidos {
+    
+    private int pedidoID;
+    private Date fechaPedido;
+    private String nombreCliente;
+    private String correoCliente;
+    private int cantidad;
+    
+    private int categoriaId;
+    private Productos productoIde;
+ 
+}
---
+++ src/main/java/entidades/Productos.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidades;
+
+import java.util.Date;
+
+/**
+ *
+ * @author IGABRIEL
+ */
+public class Productos {
+    
+    private int ProductoID;
+    private String nombre;
+    private String descripcion;
+    private String Categoria;
+    private double precio;
+
+    public Productos() {
+    }
+
+    public Productos(int ProductoID, String nombre, String descripcion, String Categoria, double precio) {
+        this.ProductoID = ProductoID;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.Categoria = Categoria;
+        this.precio = precio;
+    }
+
+    public int getProductoID() {
+        return ProductoID;
+    }
+
+    public void setProductoID(int ProductoID) {
+        this.ProductoID = ProductoID;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getCategoria() {
+        return Categoria;
+    }
+
+    public void setCategoria(String Categoria) {
+        this.Categoria = Categoria;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+       
+}
