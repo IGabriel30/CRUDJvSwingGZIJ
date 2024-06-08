b5704d286c6d6f59181f7deb64d3a5ecfd8d05c2
Ingrid Jacqueline Gabriel Zepeda <jacquelinegabriel05@hotmail.com>
2024-06-08 09:27:54 -0600

    metodo overrida toString

--- src/main/java/entidades/Productos.java
+++ src/main/java/entidades/Productos.java
@@ -68,5 +68,10 @@ public class Productos {
     public void setPrecio(double precio) {
         this.precio = precio;
     }
+    
+    @Override
+    public String toString() {
+        return nombre;
+    }
        
 }
