57ad7aaba9b3626ba59e7838538a8a9caaad0719
Ingrid Jacqueline Gabriel Zepeda <jacquelinegabriel05@hotmail.com>
2024-06-07 22:01:41 -0600

    first commit

---
+++ src/main/java/com/mycompany/crudjvswinggz/CRUDJVSwingGZ.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.crudjvswinggz;
+
+/**
+ *
+ * @author IGABRIEL
+ */
+public class CRUDJVSwingGZ {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
