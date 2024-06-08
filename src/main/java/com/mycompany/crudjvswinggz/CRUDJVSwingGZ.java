d5b7cb840473b34d247e04fc4a3101e0c00bce62
Ingrid Jacqueline Gabriel Zepeda <jacquelinegabriel05@hotmail.com>
2024-06-07 22:20:34 -0600

    se crea form de inicio

--- src/main/java/com/mycompany/crudjvswinggz/CRUDJVSwingGZ.java
+++ src/main/java/com/mycompany/crudjvswinggz/CRUDJVSwingGZ.java
@@ -11,6 +11,7 @@ package com.mycompany.crudjvswinggz;
 public class CRUDJVSwingGZ {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        FrmInicio frmInicio= new FrmInicio();
+       frmInicio.setVisible(true);
     }
 }
